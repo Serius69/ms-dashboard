ff26b61e748b15ba3a1cd28fe99d4c5cb975dc8d
cdmmucb <carlos.mendizabal@ucb.edu.bo>
2022-06-01 20:06:15 -0400

    apis casi?

---
+++ src/main/java/ucb/edu/arqsoft/msdashboard/controller/OrderController.java
@@ -0,0 +1,53 @@
+package ucb.edu.arqsoft.msdashboard.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import ucb.edu.arqsoft.msdashboard.dao.OrderRepository;
+import ucb.edu.arqsoft.msdashboard.dao.ProductRepository;
+import ucb.edu.arqsoft.msdashboard.service.ProductService;
+
+import java.util.List;
+
+@CrossOrigin("https://localhost:4200")
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+
+    private OrderRepository orderRepository;
+
+    @Autowired
+    public OrderController(OrderRepository orderRepository){
+        this.orderRepository = orderRepository;
+    }
+
+    @RequestMapping(value="/prendasmes", method = RequestMethod.GET)
+    public ResponseEntity<Integer> getPrendasMes(){
+        Integer a = orderRepository.getPrendasMes();
+        return new ResponseEntity<>(a, HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/ordenesmes", method = RequestMethod.GET)
+    public ResponseEntity<Integer> getOrdenesMes(){
+        Integer a = orderRepository.getOrdenesMes();
+        return new ResponseEntity<>(a, HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/montomes", method = RequestMethod.GET)
+    public ResponseEntity<Float> getMontoMes(){
+        Float a = orderRepository.getMontoMes();
+        return new ResponseEntity<>(a, HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/cantidadprendasmes", method = RequestMethod.GET)
+    public ResponseEntity<Integer> getCantidadPrendasMes(){
+        Integer a = orderRepository.getCantidadPrendasMes();
+        return new ResponseEntity<>(a, HttpStatus.OK);
+    }
+
+
+}
--- src/main/java/ucb/edu/arqsoft/msdashboard/controller/ProductController.java
+++ src/main/java/ucb/edu/arqsoft/msdashboard/controller/ProductController.java
@@ -1,9 +1,16 @@
 package ucb.edu.arqsoft.msdashboard.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import ucb.edu.arqsoft.msdashboard.dao.ProductRepository;
 import ucb.edu.arqsoft.msdashboard.service.ProductService;
+
+import java.util.List;
 
 @CrossOrigin("https://localhost:4200")
 @RestController
@@ -11,4 +18,26 @@ import ucb.edu.arqsoft.msdashboard.service.ProductService;
 public class ProductController {
     private ProductService productService;
 
+    private ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    @RequestMapping(value="/prendassinstock", method = RequestMethod.GET)
+    public ResponseEntity<Integer> getPrendasSinStock(){
+        Integer a = productRepository.getPrendasSinStock();
+        return new ResponseEntity<>(a, HttpStatus.OK);
+    }
+    @RequestMapping(value="/prendasconstock", method = RequestMethod.GET)
+    public ResponseEntity<Integer> getPrendasConStock(){
+        Integer a = productRepository.getPrendasConStock();
+        return new ResponseEntity<>(a, HttpStatus.OK);
+    }
+    @RequestMapping(value="/prendasporcategoria", method = RequestMethod.GET)
+    public ResponseEntity<List<Integer>> getPrendasPorCategoria(){
+        List<Integer> a = productRepository.getPrendasPorCategoria();
+        return new ResponseEntity<>(a, HttpStatus.OK);
+    }
 }
